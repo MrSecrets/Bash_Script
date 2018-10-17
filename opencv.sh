f2d402e3edb68c136c810db50e4bbf860f97eb17
Sreeraj S J <31489737+MrSecrets@users.noreply.github.com>
2018-10-17 16:46:31 +0530

    Add files via upload

--- opencv.sh
+++ opencv.sh
@@ -1,6 +1,6 @@
 # install opencv
-cd
 
+cd
 sudo apt-get update
 sudo apt-get upgrade
 
@@ -8,29 +8,29 @@ sudo apt-get upgrade
 sudo apt-get remove x264 libx264-dev
 
 # install dependencies
-sudo apt-get install build-essential checkinstall cmake pkg-config yasm
-sudo apt-get install git gfortran
-sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev
+sudo apt-get install build-essential checkinstall cmake pkg-config yasm -y
+sudo apt-get install git gfortran 
+sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev -y
 
 # 
-sudo apt-get install libtiff5-dev
+sudo apt-get install libtiff5-dev -y
  
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
-sudo apt-get install libxine2-dev libv4l-dev
-sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
-sudo apt-get install libvorbis-dev libxvidcore-dev
-sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt-get install x264 v4l-utils
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev -y
+sudo apt-get install libxine2-dev libv4l-dev -y
+sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev -y
+sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev -y
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev -y
+sudo apt-get install libvorbis-dev libxvidcore-dev -y
+sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev -y
+sudo apt-get install x264 v4l-utils -y
 
 # Optional dependencies
-sudo apt-get install libprotobuf-dev protobuf-compiler
-sudo apt-get install libgoogle-glog-dev libgflags-dev
-sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+sudo apt-get install libprotobuf-dev protobuf-compiler -y
+sudo apt-get install libgoogle-glog-dev libgflags-dev -y
+sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen -y
 
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get install python-dev python-pip python3-dev python3-pip -y
 sudo -H pip2 install -U pip numpy
 sudo -H pip3 install -U pip numpy
 
@@ -94,7 +94,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 # find out number of CPU cores in your machine
 nproc
 # substitute 4 by output of nproc
-make -j4
+sudo make -j$(nproc)
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
